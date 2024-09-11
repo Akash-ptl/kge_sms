0e3f33e90fca0e279d6895d14949f022ece0a8f9
Akash-ptl <akashkalathiya46@gmail.com>
2024-09-11 18:34:49 +0530

    Final

--- lib/Home/messages.dart
+++ lib/Home/messages.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:cron/cron.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
@@ -10,30 +9,17 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:sim_data/sim_model.dart';
 import 'package:sms/Api/api.dart';
 import 'package:sms/utils/const.dart';
-
 import '../global.dart';
 
 class MessageList extends StatefulWidget {
-  MessageList({super.key, required this.simCard});
+  const MessageList({super.key, required this.simCard});
   final List<SimCard> simCard;
-
   @override
   State<MessageList> createState() => _MessageListState();
 }
 
 class _MessageListState extends State<MessageList> {
   RxBool loading = false.obs;
-  bool _isRunning = false;
-  // @override
-  // void initState() {
-  //   // TODO: implement initState
-  //   super.initState();
-  //   loading(true);
-  //   SMSApi.sendApiRequest().then((value) {
-  //     loading(false);
-  //   });
-  // }
-  // Timer? _timer;
 
   @override
   void initState() {
@@ -42,12 +28,9 @@ class _MessageListState extends State<MessageList> {
     // cron!.schedule(Schedule.parse('*/2 * * * *'), () async {
     //   print(DateTime.now());
     //   await SMSApi.sendApiRequest().then((value) async {
-    //     _loadData(2);
+    //     _loadData();
     //   });
     // });
-    SMSApi.sendApiRequest().then((value) async {
-      _loadData(1);
-    });
   }
 
   @override
@@ -55,50 +38,20 @@ class _MessageListState extends State<MessageList> {
     super.dispose();
   }
 
-  void _loadData(type) async {
-    // loading(true);
-
+  void _loadData() async {
     if (SMSApi.getSMBDataList.value.rows?[0].type == '0') {
-      // await SMSApi.addSmbData(
-      //         SMSApi.getSMBDataList.value.rows?[0].uuid,
-      //         SMSApi.getSMBDataList.value.rows?[0].senderid,
-      //         SMSApi.getSMBDataList.value.rows?[0].receiverid,
-      //         SMSApi.getSMBDataList.value.rows?[0].msgtext)
-      //     .then(
-      //   (value) async {
       String time = DateTime.now().toLocal().toString();
-      await SMSApi.updateData(context, DateTime.now().toLocal().toString(),
-              SMSApi.getSMBDataList.value.rows?[0].uuid)
-          .then(
+      await SMSApi.updateData(context, DateTime.now().toLocal().toString(), SMSApi.getSMBDataList.value.rows?[0].uuid).then(
         (value) {
-          sendSms(
-                  SMSApi.getSMBDataList.value.rows?[0].msgtext ?? '',
-                  SMSApi.getSMBDataList.value.rows?[0].receiverid ?? '',
-                  SMSApi.getSMBDataList.value.rows?[0].uuid ?? '',
+          sendSms(SMSApi.getSMBDataList.value.rows?[0].msgtext ?? '', SMSApi.getSMBDataList.value.rows?[0].receiverid ?? '', SMSApi.getSMBDataList.value.rows?[0].uuid ?? '',
                   SMSApi.getSMBDataList.value.rows?[0].type ?? '')
               .then(
             (value) async {
               print('updateData :: ${value}');
               if (value) {
-                await SMSApi.updateData2(
-                        context,
-                        time.toString().trim(),
-                        SMSApi.getSMBDataList.value.rows?[0].uuid ?? '',
-                        DateTime.now().toLocal().toString(),
-                        1)
-                    .then((value) async {
-                  ///abc
-                });
+                await SMSApi.updateData2(context, time.toString().trim(), SMSApi.getSMBDataList.value.rows?[0].uuid ?? '', DateTime.now().toLocal().toString(), 1).then((value) async {});
               } else {
-                await SMSApi.updateData2(
-                        context,
-                        time.toString().trim(),
-                        SMSApi.getSMBDataList.value.rows?[0].uuid ?? '',
-                        'INVALID NUMBER',
-                        2)
-                    .then((value) async {
-                  ///abc
-                });
+                await SMSApi.updateData2(context, time.toString().trim(), SMSApi.getSMBDataList.value.rows?[0].uuid ?? '', 'INVALID NUMBER', 2).then((value) async {});
               }
             },
           );
@@ -109,7 +62,7 @@ class _MessageListState extends State<MessageList> {
     } else {
       sendSms('BAL', '199', '', '').then((value) async {
         if (value) {
-          Future.delayed(Duration(minutes: 1)).then((value) async {
+          Future.delayed(const Duration(minutes: 1)).then((value) async {
             var permission = await Permission.sms.status;
             if (permission.isGranted) {
               final messages = await _query.querySms(
@@ -118,11 +71,7 @@ class _MessageListState extends State<MessageList> {
                   // SmsQueryKind.sent,
                 ],
               );
-              await SMSApi.updateData3(
-                      context,
-                      SMSApi.getSMBDataList.value.rows?[0].sent ?? '',
-                      SMSApi.getSMBDataList.value.rows?[0].uuid ?? '',
-                      SMSApi.getSMBDataList.value.rows?[0].delivered ?? '',
+              await SMSApi.updateData3(context, SMSApi.getSMBDataList.value.rows?[0].sent ?? '', SMSApi.getSMBDataList.value.rows?[0].uuid ?? '', SMSApi.getSMBDataList.value.rows?[0].delivered ?? '',
                       messages.first.body)
                   .then((value) async {});
               setState(() => _messages.add(messages.first));
@@ -131,13 +80,7 @@ class _MessageListState extends State<MessageList> {
             }
           });
         } else {
-          await SMSApi.updateData3(
-                  context,
-                  SMSApi.getSMBDataList.value.rows?[0].sent ?? '',
-                  SMSApi.getSMBDataList.value.rows?[0].uuid ?? '',
-                  '',
-                  'INVALID NUMBER')
-              .then((value) async {});
+          await SMSApi.updateData3(context, SMSApi.getSMBDataList.value.rows?[0].sent ?? '', SMSApi.getSMBDataList.value.rows?[0].uuid ?? '', '', 'INVALID NUMBER').then((value) async {});
         }
       });
     }
@@ -177,37 +120,16 @@ class _MessageListState extends State<MessageList> {
       String jsonEncoded = json.encode(smsData);
       print('jsonEncoded :: ${jsonEncoded}');
 
-      // Store the sent SMS in a cache to prevent duplicates
       final sentSmsCache = <String, bool>{};
       sentSmsCache[uuid] = true;
 
-      // Show a success snackbar
-      // ScaffoldMessenger.of(context).showSnackBar(
-      //   SnackBar(
-      //     content: Text(
-      //         'SMS sent successfully to $number using SIM ${_simIndex + 1}!'),
-      //     duration: const Duration(seconds: 2),
-      //   ),
-      // );
-
-      // Move to the next SIM card for the next SMS
       _simIndex = (_simIndex + 1) % widget.simCard.length;
 
-      return true; // Return true if the SMS is sent successfully
+      return true;
     } catch (e) {
-      // Show an error snackbar
-      // ScaffoldMessenger.of(context).showSnackBar(
-      //   SnackBar(
-      //     content: Text(
-      //         'Error sending SMS to $number using SIM ${_simIndex + 1}: INVALID NUMBER'),
-      //     duration: const Duration(seconds: 2),
-      //   ),
-      // );
-
-      // Move to the next SIM card for the next SMS
       _simIndex = (_simIndex + 1) % widget.simCard.length;
 
-      return false; // Return false if there's an error sending the SMS
+      return false;
     }
   }
 
@@ -237,7 +159,7 @@ class _MessageListState extends State<MessageList> {
                 cron!.schedule(Schedule.parse('*/2 * * * *'), () async {
                   print(DateTime.now());
                   await SMSApi.sendApiRequest().then((value) async {
-                    _loadData(2);
+                    _loadData();
                   });
                 });
               } else {
@@ -245,9 +167,7 @@ class _MessageListState extends State<MessageList> {
                 cron = null;
               }
             },
-            child: (start == false)
-                ? const Icon(Icons.play_arrow)
-                : const Icon(Icons.stop),
+            child: (start == false) ? const Icon(Icons.play_arrow) : const Icon(Icons.stop),
           ),
           2.pw,
           FloatingActionButton(
@@ -269,15 +189,15 @@ class _MessageListState extends State<MessageList> {
             child: const Icon(Icons.refresh),
           ),
           2.pw,
-          // FloatingActionButton(
-          //   heroTag: 2,
-          //   child: const Icon(Icons.send),
-          //   onPressed: () async {
-          //     SMSApi.sendApiRequest().then((value) async {
-          //       _loadData(1);
-          //     });
-          //   },
-          // ),
+          FloatingActionButton(
+            heroTag: 2,
+            child: const Icon(Icons.send),
+            onPressed: () async {
+              SMSApi.sendApiRequest().then((value) async {
+                _loadData();
+              });
+            },
+          ),
         ],
       ),
       body: Obx(() {
@@ -288,54 +208,18 @@ class _MessageListState extends State<MessageList> {
                   child: Column(
                     children: [
                       10.ph,
-                      // ListView.builder(
-                      //   physics: NeverScrollableScrollPhysics(),
-                      //   itemCount:
-                      //       SMSApi.getSMBDataList.value.rows?.length ?? 0,
-                      //   shrinkWrap: true,
-                      //   itemBuilder: (context, index) {
-                      //     final data = SMSApi.getSMBDataList.value.rows?[index];
-                      //     return Card(
-                      //         child: Padding(
-                      //       padding: const EdgeInsets.symmetric(
-                      //           horizontal: 15, vertical: 15),
-                      //       child: Container(
-                      //           // height: 50,
-                      //           child: Column(
-                      //         crossAxisAlignment: CrossAxisAlignment.start,
-                      //         children: [
-                      //           Text(
-                      //             data?.deviceid ?? ' ',
-                      //             style: const TextStyle(
-                      //                 fontSize: 16,
-                      //                 fontWeight: FontWeight.bold),
-                      //           ),
-                      //           0.5.ph,
-                      //           Text('MsgText : ${data?.msgtext ?? ' '}'),
-                      //           0.5.ph,
-                      //           Text('To : ${data?.receiverid ?? ' '}'),
-                      //           0.5.ph,
-                      //           Text('From : ${data?.senderid ?? ' '}'),
-                      //           0.5.ph,
-                      //           Text('Type : ${data?.type ?? ' '}'),
-                      //         ],
-                      //       )),
-                      //     ));
-                      //   },
-                      // ),
-                      Divider(
+                      const Divider(
                         thickness: 3,
                       ),
-                      Text(
+                      const Text(
                         'Inbox Messages',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 25),
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
                       ),
-                      Divider(
+                      const Divider(
                         thickness: 3,
                       ),
                       ListView.builder(
-                        physics: NeverScrollableScrollPhysics(),
+                        physics: const NeverScrollableScrollPhysics(),
                         shrinkWrap: true,
                         itemCount: _messages.length,
                         itemBuilder: (BuildContext context, int i) {
@@ -355,7 +239,7 @@ class _MessageListState extends State<MessageList> {
                                 Text('dateSent : ${message.dateSent} '),
                                 Text('date : ${message.date} '),
                                 Text('Msg : ${message.body}'),
-                                Divider()
+                                const Divider()
                               ],
                             ),
                           );
